a38ca4b28e2e0846fd4786adff2e5ab2c6eb9cbd
carlos celi <normando1945@gmail.com>
2026-03-23 08:02:21 -0500

    new class

--- repo_maxtrix_analisys/__init__.py
+++ repo_maxtrix_analisys/__init__.py
@@ -1,7 +1,8 @@
-from .core import MF_K_T_L_Element2D, MF_L_elements2D, M_visual_2D_3D
+from .core import MF_K_T_L_Element2D, MF_L_elements2D, M_visual_2D_3D, SimpleMatrixStack
 
 __all__ = [
     "MF_K_T_L_Element2D",
     "MF_L_elements2D",
     "M_visual_2D_3D",
+    "SimpleMatrixStack",
 ]
