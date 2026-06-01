196e7f8f1a62b94f124d0658a972f7aed1135fff
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2026-06-01 08:27:02 -0500

    update repo

--- repo_maxtrix_analisys/__init__.py
+++ repo_maxtrix_analisys/__init__.py
@@ -1,6 +1,6 @@
 from .core import (MF_K_T_L_Element2D, Manager_K_T_elements2D, M_visual_2D_3D, 
                    SimpleMatrixStack, StiffnessMatrix_simple,
-                   Manual_Flexural_Method, ARM_K_T_Element2D)
+                   Manual_Flexural_Method, ARM_K_T_Element2D, Assembler)
 
 __all__ = [
     "MF_K_T_L_Element2D",
@@ -9,5 +9,6 @@ __all__ = [
     "SimpleMatrixStack",
     "StiffnessMatrix_simple",
     "Manual_Flexural_Method",
-    "ARM_K_T_Element2D"
+    "ARM_K_T_Element2D",
+    "Assembler"
 ]
